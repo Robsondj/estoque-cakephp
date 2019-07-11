1e23b79becb240235392563b7c14ab85d833fcec
Robsondj <robson.djsilva@gmail.com>
2019-07-11 13:31:33 -0300

    Adicionado listagem de produtos estática

--- src/Controller/ProdutosController.php
+++ src/Controller/ProdutosController.php
@@ -10,7 +10,9 @@ class ProdutosController extends AppController
 	
 	function index()
 	{
-		$msg = "Bem vindo Produtos";
-		$this->set('msg', $msg);
+		$produtos = [];
+		$produtos[] = ["id" => 1,"nome" => "HD 20 gigas","preco" => 29.99,"descricao" => "HD muito bom da marca mega-HD"];
+		$produtos[] = ["id" => 2,"nome" => "TV","preco" => 89.99,"descricao" => "TV muito boa da marca Sony"];
+		$this->set('produtos', $produtos);
 	}
 }
--- src/Template/Produtos/index.ctp
+++ src/Template/Produtos/index.ctp
@@ -1,1 +1,20 @@
-<h1><?= $msg; ?></h1>
+<table>
+	<thead>
+		<tr>
+			<td>Id</td>
+			<td>Nome</td>
+			<td>Preço</td>
+			<td>Descrição</td>
+		</tr>
+	</thead>
+	<tbody>
+	<?php foreach($produtos as $produto) { ?>
+		<tr>
+			<td><?= $produto['id'] ?></td>
+			<td><?= $produto['nome'] ?></td>
+			<td><?= $produto['preco'] ?></td>
+			<td><?= $produto['descricao'] ?></td>
+		</tr>
+	<?php } ?>
+	</tbody>
+</table>
